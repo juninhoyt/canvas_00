66bbec556264d81e63df780fd01f4c6813218720
juninhoyt <113396677+juninhoyt@users.noreply.github.com>
2023-09-12 16:18:41 -0300

    animação das bolinhas

--- canvas.js
+++ canvas.js
@@ -1,15 +1,27 @@
 var canvas = document.getElementById("canvas01")
 var tela = canvas.getContext("2d")
 
-tela.fillStyle = "blue"
-tela.strokeStyle = "cyan"
+setInterval(ma,100)
 
-for(var m=0;m<50;m++){
-    var x = Math.random()*1000
-    var y = Math.random()*600
-    tela.beginPath()
-    tela.arc(x , y , 100 , 0 , 360)
-    tela.fill()
-    tela.stroke()
+function ma(){
+
+    tela.clearRect(0,0,1000,600)
+
+    for(var m=0;m<50;m++){
+
+        var red = Math.random()*255
+        var green = Math.random()*255
+        var blue = Math.random()*255
+    
+        tela.fillStyle = "rgb("+red+","+green+","+blue+")"
+        tela.strokeStyle = "cyan"
+    
+        var x = Math.random()*1000
+        var y = Math.random()*600
+        tela.beginPath()
+        tela.arc(x , y , 50 , 0 , 360)
+        tela.fill()
+        tela.stroke()
+    }
+    
 }
-
